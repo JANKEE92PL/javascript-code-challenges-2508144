9ca4ba17199e0e67514ff285767e157f93541eec
Sebastian <jankee112@googlemail.com>
2023-05-08 12:27:41 +0000

    solve challenge 06howold.js

--- challenges/06howold.js
+++ challenges/06howold.js
@@ -1,5 +1,8 @@
 function howOld(dates) {
-  // Write your code here
+  const diff = dates.map(Date.parse).reduce((prev, curr) => prev - curr)
+  const years = new Date(Math.abs(diff)).getFullYear() - 1970;
+
+  return years
 
 }
 
@@ -10,6 +13,6 @@ describe("Tests", () => {
     assert.equal(howOld(['1. January 2025', '31 December 2024']), 0);
     assert.equal(howOld(['31 December 2024', '1. January 2025']), 0);
     assert.equal(howOld(['12 May 2024', '11 May 2034']), 9);
-    assert.equal(howOld(['12 May 2024', '12 May 2034']), 10);  
+    assert.equal(howOld(['12 May 2024', '12 May 2034']), 10);
   });
 });
